825946d1c2ce901c9c7dce4fe1143e29d5933066
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2024-03-25 00:38:01 +0800

    fixed docker file

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,9 @@ from flask import request, jsonify
 from config import app, db
 from models import Contact
 
+@app.route("/", methods=["GET"])
+def test():
+    return "Hello World"
 
 @app.route("/contacts", methods=["GET"])
 def get_contacts():
@@ -66,4 +69,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
